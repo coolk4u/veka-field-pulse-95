aff1ce1ac58c8393ee179fc2bd786baa07769078
DEBAJYOTI GHOSH <91932081+DGRYZER@users.noreply.github.com>
2025-07-14 11:39:58 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,11 @@ import VisitDetail from "./pages/VisitDetail";
 import AttendanceDetail from "./pages/AttendanceDetail";
 import ConveyanceDetail from "./pages/ConveyanceDetail";
 import NotFound from "./pages/NotFound";
+import FetchData from "./FetchData";
+import Leads from "./pages/Leads";
+import LeadDetail from "./pages/LeadDetail";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const queryClient = new QueryClient();
 
@@ -19,8 +24,11 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+      {/* <FetchData /> */}
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/leads" element={<Leads />} />
+          <Route path="/lead-detail/:id" element={<LeadDetail />} />
           <Route path="/visits" element={<Visits />} />
           <Route path="/visit/:id" element={<VisitDetail />} />
           <Route path="/attendance" element={<AttendanceDetail />} />
@@ -29,6 +37,7 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+      <ToastContainer position="top-right" autoClose={3000} />
     </TooltipProvider>
   </QueryClientProvider>
 );
